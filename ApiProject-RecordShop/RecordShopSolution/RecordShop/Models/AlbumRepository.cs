9cb493085b5d375b15321c61d0559526ff26b948
Zitian Harper <Zitian.harper@icloud.com>
2026-05-05 14:29:21 +0100

    added appsettings.Development and updated AlbumsFilePath

--- ApiProject-RecordShop/RecordShopSolution/RecordShop/Models/AlbumRepository.cs
+++ ApiProject-RecordShop/RecordShopSolution/RecordShop/Models/AlbumRepository.cs
@@ -1,14 +1,19 @@
 ﻿using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+
 
 namespace RecordShop.Models
 {
     public class AlbumRepository : IAlbumRepository
     {
-        private readonly string _filePath = Path.Combine("Data", "Albums.json");
+        //private readonly string _filePath = Path.Combine("Data", "Albums.json");
+        private readonly string _filePath;
         private readonly List<Album> _album;
 
-        public AlbumRepository()
+        public AlbumRepository(IConfiguration config)
         {
+            _filePath = config["FileSettings:AlbumFilePath"]; //this connects to appsettings.Development 
+
             var json = File.ReadAllText(_filePath);
             _album = JsonSerializer.Deserialize<List<Album>>(json) ?? new List<Album>();
         }
