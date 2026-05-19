02dde6b091a8f28adb18fcc05a33058190967836
Zitian Harper <Zitian.harper@icloud.com>
2026-05-19 17:53:21 +0100

    Add Blazor RecordShop Web project

---
+++ ApiProject-RecordShop/RecordShopSolution/RecordShop.Web/RecordShop.Web/Models/Album.cs
@@ -0,0 +1,12 @@
+﻿namespace RecordShop.Web.Models;
+
+public class Album
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Artist { get; set; } = string.Empty;
+    public int Release { get; set; }
+    public string Genre { get; set; } = string.Empty;
+    public int StockQuantity { get; set; }
+    public decimal Price { get; set; }
+}
